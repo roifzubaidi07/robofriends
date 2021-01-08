6bdb31241a994a23e21983ff35f0a829aa719fab
M. Ro'if <roifzubaidi5@gmail.com>
2021-01-08 16:08:41 +0700

    Backend apps

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({email,name,id}) => {
+    return (
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="robot" src={`https://robohash.org/${id}`} width="150px" height="150px"/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
